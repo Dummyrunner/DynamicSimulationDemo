e970de8fa20cc4c657b3c267aa3f1c75d8965713
Daniel Bergmann <danielbergmann@posteo.de>
2025-11-15 00:56:00 +0100

    configure IO of boat plant

--- boat_top_down_plant.py
+++ boat_top_down_plant.py
@@ -1,24 +1,34 @@
 import pymunk
 import pymunk.pygame_util
+import math_helpers
 from pymunk import Vec2d
 from typing import NamedTuple
 from abc import ABC, abstractmethod
 from physical_objects import BoatTopDown
+import pygame
 
 BOAT_SPEED = 20
+STEERING_TORQUE = 1e7
+BOAT_LENGTH = 25
+BOAT_WIDTH = 10
 
 
 # Define input/output types as class attributes using nested classes
 class BoatTopdownOutput(NamedTuple):
-    pass
+    x_distance: float
+    x_velocity: float
+    angle: float
 
 
 class BoatTopdownInput(NamedTuple):
-    pass
+    steering_torque: float
 
 
 class BoatTopdownState(NamedTuple):
-    pass
+    x_distance: float
+    x_velocity: float
+    angle: float
+    angular_velocity: float
 
 
 class PlantBase(ABC):
@@ -49,18 +59,36 @@ class BoatTopdownPlant(PlantBase):
         self.n_outputs: int = 4
         self.control_active = True
         self._create_objects(window_size, space)
-        self.input = BoatTopdownInput()
-        self.output = BoatTopdownOutput()
-        self.state = BoatTopdownState()
+        self.input = BoatTopdownInput(0)
+        self.output = BoatTopdownOutput(0, 0, 0)
+        self.state = BoatTopdownState(0, 0, 0, 0)
+        self.window_with = window_size[0]
 
     def step(self, time_delta):
+        self.boat.body.apply_force_at_local_point(
+            self.input.steering_torque, Vec2d(0, BOAT_LENGTH / 2)
+        )
         self.space.step(time_delta)
 
     def get_state(self) -> "BoatTopdownState":
-        return BoatTopdownState(0)
+        boat_body = self.boat.body
+        angle = self._calculate_angle_radian()
+        angular_velocity = boat_body.angular_velocity
+        x_center = self.window_with / 2
+        x_distance = boat_body.position - x_center
+        x_velocity = boat_body.velocity.x
+        return BoatTopdownState(
+            x_distance=x_distance,
+            x_velocity=x_velocity,
+            angle=angle,
+            angular_velocity=angular_velocity,
+        )
 
     def get_output(self) -> "BoatTopdownOutput":
-        pass
+        state = self.state
+        return BoatTopdownOutput(
+            x_distance=state.x_distance, x_velocity=state.x_velocity, angle=state.angle
+        )
 
     def set_input(self, input_data) -> None:
         self.input = input_data
@@ -69,16 +97,31 @@ class BoatTopdownPlant(PlantBase):
         pass
 
     def force_from_key_input(self) -> Vec2d:
-        """Calculate new velocity based on input and constraints
-
+        """
         Returns:
             Vec2d: New velocity vector (x, y)
         """
-        pass
+        keys = pygame.key.get_pressed()
+        print(keys[pygame.K_LEFT])
+        print("key?")
+        if keys[pygame.K_LEFT]:
+            return Vec2d(-STEERING_TORQUE, 0)
+        elif keys[pygame.K_RIGHT]:
+            return Vec2d(STEERING_TORQUE, 0)
+        else:
+            return Vec2d(0, 0)
 
     def _create_objects(self, window_size, space):
         pos_x = window_size[0] // 2
         pos_y = window_size[1] * 0.8
-        self.boat = BoatTopDown(space, position=(pos_x, pos_y), length=25, width=10)
+        self.boat = BoatTopDown(
+            space, position=(pos_x, pos_y), length=BOAT_LENGTH, width=BOAT_WIDTH
+        )
         self.all_physical_objects.append(self.boat)
         self.boat.body.velocity = Vec2d(0, -BOAT_SPEED)
+
+    def _calculate_angle_radian(self):
+        # Calculate vector from runner to ball
+        boat_angle = self.boat.body.angle
+        vertical_up = Vec2d(0, -1)
+        return math_helpers.angle_between_vectors_radian(boat_angle, vertical_up)
